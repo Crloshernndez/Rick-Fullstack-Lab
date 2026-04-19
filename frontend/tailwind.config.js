481aa50a46ed7bab7543a03a6759e87895574336
carlos hernandez <hernandez1989carlos@gmail.com>
2026-04-19 10:12:52 -0500

    feat(frontend): configurate Tailwind config and App.css and index.css files

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "brand-light": "#EEE3FF", // Lavanda suave
+        "brand-primary": "#8054C7", // Morado principal
+        "brand-dark": "#5A3696", // Morado profundo
+        "brand-success": "#63D838", // Verde vibrante (perfecto para botones de acción)
+      },
+      fontFamily: {
+        greycliff: ["GreycliffCF", "sans-serif"],
+      },
+      screens: {
+        xs: "375px",
+      },
+    },
   },
   plugins: [],
 };
